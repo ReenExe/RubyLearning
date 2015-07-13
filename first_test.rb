eddeff4ac816bfc85eca43f5c314dc3fb66e1e2a
ReenExe <ipourri@gmail.com>
2015-07-13 21:53:07 +0300

    progress

---
+++ first_test.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+
+class MyTest < Test::Unit::TestCase
+
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test_success
+    assert_true(true)
+  end
+end
