7b7c96961f5ab6bc7a7e90f731a5b2a7235a1fd6
ReenExe <ipourri@gmail.com>
2015-07-13 23:27:35 +0300

    multi assign

---
+++ test/assign_test.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+class AssignTest < Test::Unit::TestCase
+
+  def test_multi
+    a, b, c = [1, 2, 3]
+
+    assert_equal(a, 1)
+    assert_equal(b, 2)
+    assert_equal(c, 3)
+  end
+
+end
