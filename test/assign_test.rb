9568bf574d3b174dfd002e399cc269a65ee6ec47
ReenExe <ipourri@gmail.com>
2015-07-13 23:29:55 +0300

    assign exchange

--- test/assign_test.rb
+++ test/assign_test.rb
@@ -4,10 +4,16 @@ class AssignTest < Test::Unit::TestCase
 
   def test_multi
     a, b, c = [1, 2, 3]
-
     assert_equal(a, 1)
     assert_equal(b, 2)
     assert_equal(c, 3)
+  end
+
+  def test_exchange
+    a, b = 1, 2
+    a, b = b, a
+    assert_equal(a, 2)
+    assert_equal(b, 1)
   end
 
 end
