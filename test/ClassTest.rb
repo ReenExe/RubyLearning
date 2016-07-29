d1d7a4d21306150e414c6843504787eeb7764095
ReenExe <ipourri@gmail.com>
2016-07-29 20:43:17 +0300

    class Range

--- test/ClassTest.rb
+++ test/ClassTest.rb
@@ -9,6 +9,12 @@ class ClassTest < Test::Unit::TestCase
     assert_equal(nil.class, NilClass)
     assert_equal([].class, Array)
     assert_equal({}.class, Hash)
+
+    sequence = 1..2
+    assert_equal(sequence.class, Range)
+
+    sequence = 1...2
+    assert_equal(sequence.class, Range)
   end
 
 end
