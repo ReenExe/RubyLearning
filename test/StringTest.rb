71e7fd628850ffd20bb56e06fa849e9b987b07fd
ReenExe <ipourri@gmail.com>
2016-07-29 20:41:22 +0300

    progress String testing

--- test/StringTest.rb
+++ test/StringTest.rb
@@ -1,7 +1,13 @@
 class StringTest < Test::Unit::TestCase
 
-  def testEqual
-    assert_equal('value', 'value')
-  end
+    def testEqual
+        assert_equal('value', 'value')
+    end
+
+    def testGetIndex
+        name = 'Reen';
+        assert_equal(name[0], 'R')
+        assert_equal(name[-1], 'n')
+    end
 
 end
