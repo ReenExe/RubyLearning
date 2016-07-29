ef093aa3f4db4a6aa4d08906183dca7388325301
ReenExe <ipourri@gmail.com>
2016-07-29 20:46:43 +0300

    progress

--- test/StringTest.rb
+++ test/StringTest.rb
@@ -10,7 +10,14 @@ class StringTest < Test::Unit::TestCase
         assert_equal(name.size, 7);
 
         assert_equal(name[0], 'R')
+        assert_equal(name[6], 'e')
+        assert_equal(name[-7], 'R')
         assert_equal(name[-1], 'e')
+
+        assert_equal(name[0...7], 'ReenExe')
+
+        assert_equal(name[8], nil)
+        assert_equal(name[-8], nil)
     end
 
 end
