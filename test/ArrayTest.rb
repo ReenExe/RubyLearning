67413008feb14eaf258f5bf881e0da20d8bcfcb8
ReenExe <ipourri@gmail.com>
2016-08-01 20:01:08 +0300

    array intersect or union - return unique

--- test/ArrayTest.rb
+++ test/ArrayTest.rb
@@ -1,9 +1,9 @@
 class ArrayTest < Test::Unit::TestCase
 
   def testConcat
-    concat = [1] + [2] + [3]
+    concat = [1] + [2] + [3] + [1] + [2]
 
-    assert_equal(concat, [1, 2, 3])
+    assert_equal(concat, [1, 2, 3, 1, 2])
   end
 
   def testIntersect
@@ -12,10 +12,22 @@ class ArrayTest < Test::Unit::TestCase
     assert_equal(intersect, [1, 3, 5])
   end
 
-  def testUnion
-    intersect = [1, 2, 3, 4, 5] | [1, 3, 5, 7, 9]
+  def testIntersectUnique
+    intersect = [1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3] & [1, 2, 3]
 
-    assert_equal(intersect, [1, 2, 3, 4, 5, 7, 9])
+    assert_equal(intersect, [1, 2, 3])
+  end
+
+  def testUnion
+    union = [1, 2, 3, 4, 5] | [1, 3, 5, 7, 9]
+
+    assert_equal(union, [1, 2, 3, 4, 5, 7, 9])
+  end
+
+  def testUnionUnique
+    union = [1, 2, 3, 2, 1] | [1, 3, 5, 7, 9]
+
+    assert_equal(union, [1, 2, 3, 5, 7, 9])
   end
 
 end
