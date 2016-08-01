af4c7079b4069799e0ba3e5473a476f95f2c7dc4
ReenExe <ipourri@gmail.com>
2016-08-01 19:57:59 +0300

    array union and intersect

--- test/ArrayTest.rb
+++ test/ArrayTest.rb
@@ -6,4 +6,16 @@ class ArrayTest < Test::Unit::TestCase
     assert_equal(concat, [1, 2, 3])
   end
 
+  def testIntersect
+    intersect = [1, 2, 3, 4, 5] & [1, 3, 5, 7, 9]
+
+    assert_equal(intersect, [1, 3, 5])
+  end
+
+  def testUnion
+    intersect = [1, 2, 3, 4, 5] | [1, 3, 5, 7, 9]
+
+    assert_equal(intersect, [1, 2, 3, 4, 5, 7, 9])
+  end
+
 end
